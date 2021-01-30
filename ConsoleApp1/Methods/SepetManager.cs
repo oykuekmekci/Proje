6089007c20f913904e08d5856a57f6bd97e57bd4
oykuekmekci <77547986+oykuekmekci@users.noreply.github.com>
2021-01-30 17:03:22 +0300

    Add files via upload

---
+++ ConsoleApp1/Methods/SepetManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetManager
+    {
+        //naming convention
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler, sepete eklendi : "  + urun.Adi);
+
+
+        }
+    }
+}
